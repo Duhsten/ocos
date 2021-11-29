bae020314569e46f807d4ad293dc3780a5ad0ebf
Duhsten <dustinkosborne@gmail.com>
2021-11-29 00:41:40 -0500

    +

--- External/install.lua
+++ External/install.lua
@@ -1,4 +1,8 @@
 local shell = require("shell")
 
 shell.execute("cd /bin/ ; wget -f https://raw.githubusercontent.com/Duhsten/ocos/main/bin/tar.lua")
-shell.execute("wget -f https://raw.githubusercontent.com/Duhsten/ocos/version/os.tar")
+shell.execute("cd ../ ; wget -f https://raw.githubusercontent.com/Duhsten/ocos/version/os.tar")
+shell.execute("cd ../ ; tar -f -x os.tar ; rm os.tar")
+print("Trikon OS Installed - Restarting")
+os.sleep(2)
+computer.shutdown(true)
