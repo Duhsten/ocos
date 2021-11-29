47e29d1e00d8e50a6844b47821454aee22a372c2
Duhsten <dustinkosborne@gmail.com>
2021-11-29 00:29:50 -0500

    +

--- External/install.lua
+++ External/install.lua
@@ -1,3 +1,4 @@
 local shell = require("shell")
 
-shell.execute("wget ")
+shell.execute("cd /bin/ ; wget -f https://raw.githubusercontent.com/Duhsten/ocos/main/bin/tar.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/Duhsten/ocos/version/os.tar")
